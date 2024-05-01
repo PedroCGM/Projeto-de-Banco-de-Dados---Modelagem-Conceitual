580bb56ccf922b0728e9fc0e73cde6c8fd2e1151
PedroCGM <106196558+PedroCGM@users.noreply.github.com>
2024-05-01 18:36:13 -0300

    Update SQLQuery3.sql

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,22 +1,64 @@
-USE [Loja]
-GO
+-- item (a)
+SELECT p.*, pf.cpf
+FROM Pessoa p
+INNER JOIN PessoaFisica pf ON p.idPessoa = pf.FK_Pessoa_idPessoa;
 
-/****** Object:  Table [dbo].[Produtos]    Script Date: 01/05/2024 13:43:04 ******/
-SET ANSI_NULLS ON
-GO
+-- item (b)
+SELECT p.*, pj.cnpj
+FROM Pessoa p
+INNER JOIN PessoaJuridica pj ON p.idPessoa = pj.FK_Pessoa_idPessoa;
 
-SET QUOTED_IDENTIFIER ON
-GO
+-- item (c)
+SELECT m.*, p.nome as fornecedor, pr.nome as Produto, m.quantidade, m.precoUnitario, (m.quantidade * m.precoUnitario) as total
+FROM Movimento m
+INNER JOIN Pessoa p ON p.idPessoa = m.FK_Pessoa_idPessoa
+INNER JOIN Produto pr ON pr.idProduto = m.FK_Produto_idProduto
+WHERE m.tipo = 'E';
 
-CREATE TABLE [dbo].[Produtos](
-    [idProduto] [INT] NOT NULL PRIMARY KEY,
-      NULL,
-    [quantidade] [INT] NULL,
-    [preco_venda] [DECIMAL](10, 2) NULL
-) ON [PRIMARY]
-GO
+-- item (d)
+SELECT m.*, p.nome as comprador, pr.nome as Produto, m.quantidade, m.precoUnitario, (m.quantidade * m.precoUnitario) as total
+FROM Movimento m
+INNER JOIN Pessoa p ON m.FK_Pessoa_idPessoa = p.idPessoa
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'S';
 
-INSERT INTO Produtos (idProduto, nome, quantidade, preco_venda)
-VALUES (1, 'banana', 100, 5.00),
-       (2, 'laranja', 200, 2.00),
-       (3, 'manga', 500, 4.00);
+-- item (e)
+SELECT pr.nome, SUM(m.quantidade * m.precoUnitario) as compras
+FROM Movimento m
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'E'
+GROUP BY pr.nome;
+
+-- item (f)
+SELECT pr.nome, SUM(m.quantidade * m.precoUnitario) as vendas
+FROM Movimento m
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'S'
+GROUP BY pr.nome;
+
+-- item (g)
+SELECT u.*
+FROM Usuario u
+LEFT JOIN Movimento m ON u.idUsuario = m.FK_Usuario_idUsuario AND m.tipo = 'E'
+WHERE m.idMovimento IS NULL;
+
+-- item (h)
+SELECT u.loginName, SUM(m.precoUnitario * m.quantidade) as compras
+FROM Movimento m
+INNER JOIN Usuario u ON m.FK_Usuario_idUsuario = u.idUsuario
+WHERE m.tipo = 'E'
+GROUP BY u.loginName;
+
+-- item (i)
+SELECT u.loginName, SUM(m.precoUnitario * m.quantidade) as vendas
+FROM Movimento m
+INNER JOIN Usuario u ON m.FK_Usuario_idUsuario = u.idUsuario
+WHERE m.tipo = 'S'
+GROUP BY u.loginName;
+
+-- item (j)
+SELECT pr.nome, SUM(m.precoUnitario * m.quantidade) / SUM(m.quantidade) as media
+FROM Movimento m
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'S'
+GROUP BY pr.nome;
